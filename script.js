0fba385668c1547d7b89130075e111171b5bdbb8
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-07-08 17:19:44 +0100

    Check win condition

--- script.js
+++ script.js
@@ -14,6 +14,50 @@ const gameBoard = (function() {
         board[coordX][coordY] = token;
     }
 
+    function checkWin() {
+        let gameResult;
+        // check row
+        if (!board[0].includes("")) {
+            if (board[0][0] === board [0][1] && board[0][1] === board[0][2]) {
+                gameResult = {win: true, token: board[0][0]}
+            }
+        } else if (!board[1].includes("")) {
+            if (board[1][0] === board [1][1] && board[1][1] === board[1][2]) {
+                gameResult = {win: true, token: board[1][0]}
+            }
+        } else if (!board[2].includes("")) {
+            if (board[2][0] === board [2][1] && board[0][1] === board[2][2]) {
+                gameResult = {win: true, token: board[2][0]}
+            }
+        // check column
+        } else if (board[0][0] !== "" && board[1][0] !== "" && board[2][0] !== "") {
+            if (board[0][0] === board [1][0] && board[1][0] === board[2][0]) {
+                gameResult = {win: true, token: board[0][0]}
+            }
+        } else if (board[0][1] !== "" && board[1][1] !== "" && board[2][1] !== "") {
+            if (board[0][1] === board [1][1] && board[1][1] === board[2][1]) {
+                gameResult = {win: true, token: board[0][1]}
+            }
+        } else if (board[0][2] !== "" && board[1][2] !== "" && board[2][2] !== "") {
+            if (board[0][2] === board [1][2] && board[1][0] === board[2][2]) {
+                gameResult = {win: true, token: board[0][2]}
+            }
+        // check diagonal
+        } else if (board[0][0] !== "" && board[1][1] !== "" && board[2][2] !== "") {
+            if (board[0][0] === board [1][1] && board[1][1] === board[2][2]) {
+                gameResult = {win: true, token: board[0][0]}
+            }
+        } else if (board[0][2] !== "" && board[1][1] !== "" && board[2][0] !== "") {
+            if (board[0][2] === board [1][1] && board[1][1] === board[2][0]) {
+                gameResult = {win: true, token: board[0][2]}
+            }
+        } else {
+            gameResult = {win: false, token: ""}
+        }
+
+        return gameResult;
+    }
+
     function reset() {
         board = [
             ["", "", ""],
@@ -22,19 +66,21 @@ const gameBoard = (function() {
         ]
     }
 
-    return { currentBoard, placeToken, reset }
+    return { currentBoard, placeToken, checkWin, reset }
 })();
 
 
 // Create a player object
-function createPlayer(name) {
+function createPlayer(name, token) {
+    let playerName = name;
+    let playerToken = token;
 
-    return { name };
+    return { playerName, playerToken };
 }
 
 function playGame() {
-    let playerOne = createPlayer(prompt("Player One: please enter your name"));
-    let playerTwo = createPlayer(prompt("Player Two: please enter your name"));
+    let playerOne = createPlayer(prompt("Player One: please enter your name"), "X");
+    let playerTwo = createPlayer(prompt("Player Two: please enter your name"), "O");
 
     console.log({
         playerOne: playerOne.name,
