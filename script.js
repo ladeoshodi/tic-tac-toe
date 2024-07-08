633bbeb6f01ad3e2ff3c0c74de3aed255a677514
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-07-08 16:12:56 +0100

    Create GameBoard module

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
+// create game board
+const gameBoard = (function() {
+    let board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+
+    function currentBoard() {
+        return board
+    }
+
+    function placeToken(coordX, coordY, token) {
+        board[coordX][coordY] = token;
+    }
+
+    function reset() {
+        board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ]
+    }
+
+    return { currentBoard, placeToken, reset }
+})();
+
+
 // Create a player object
-function createPlayer (name) {
+function createPlayer(name) {
 
-    return {name};
+    return { name };
 }
-
-
-
 
 function playGame() {
     let playerOne = createPlayer(prompt("Player One: please enter your name"));
@@ -17,12 +42,8 @@ function playGame() {
     })
 };
 
-
-
-
-
 // play game
 console.log("This is a tic tac toe game on the console");
 console.log("You win by ensuring that 3 of your tokens ('X' or 'O') lines up vertically, horizontally or diagonally");
 
-playGame();
+// playGame();
